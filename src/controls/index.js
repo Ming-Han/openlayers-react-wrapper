29e2335788526a8aca942aa611c19b98e06e30e7
Unknown <supershot.1020@gmail.com>
2018-05-25 16:20:35 +0800

    [update]

--- src/controls/index.js
+++ src/controls/index.js
@@ -1,7 +1,24 @@
 import FullScreen from './fullScreen';
 import Controls from './controls';
+import Attribution from './attribution';
+import MousePosition from './mousePosition';
+import OverviewMap from './overviewMap';
+import Rotate from './rotate';
+import ScaleLine from './scaleLine';
+import Zoom from './zoom';
+import ZoomSilder from './zoomSilder';
+import ZoomToExtent from './zoomToExtent';
 let Control = {
-    FullScreen : FullScreen
+    FullScreen : FullScreen,
+    Controls : Controls,
+    Attribution : Attribution,
+    MousePosition: MousePosition,
+    OverviewMap : OverviewMap,
+    Rotate : Rotate,
+    ScaleLine : ScaleLine,
+    Zoom : Zoom,
+    ZoomSilder : ZoomSilder,
+    ZoomToExtent : ZoomToExtent
 }
 
 export {
